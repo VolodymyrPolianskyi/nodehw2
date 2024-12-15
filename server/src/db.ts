902705ee68a3690a369cd1a689b72072814b46ba
Volodymyr Polianskyi <web.vladimir2006@gmail.com>
2024-12-15 12:34:22 +0100

    fixed db problems

--- server/src/db.ts
+++ server/src/db.ts
@@ -10,12 +10,15 @@ import { RenameTitleToHeader1733774515040 } from './migrations/1733774515040-Ren
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: +process.env.DB_PORT || 1488,
+  host: process.env.HOST,
+  port: +process.env.PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB,
-  synchronize: false,
+  ssl:{
+    rejectUnauthorized:false
+  },
+  synchronize: true,
   entities: [User, NewsPost],
   migrations:[AddDeletedColumns1733774555699,AddIndexesToColumns1733774489797,RenameTitleToHeader1733774515040]
 })
